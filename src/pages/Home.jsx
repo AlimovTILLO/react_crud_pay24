48f0bdd683e15272fac6604f3314d55847b5cd51
Alimov Tillo <alimovtillo@gmail.com>
2021-04-24 18:17:09 +0600

    f

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { passportActions } from '../actions';
 
 
 export function Home() {
+    const [submitted, setSubmitted] = useState(false);
     const [inputs, setInputs] = useState({
         firstname: '',
         lastname: '',
@@ -24,6 +25,7 @@ export function Home() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        setSubmitted(true);
         if (inputs) {
             dispatch(passportActions.create(inputs));
         }
@@ -35,28 +37,46 @@ export function Home() {
                 <Form.Field>
                     <label>Имя</label>
                     <Input fluid placeholder='Имя' name='firstname' value={inputs.firstname} onChange={handleChange} />
+                    {submitted && !inputs.firstname &&
+                        <div className="invalid-feedback">Введите Имя*</div>
+                    }
                 </Form.Field>
                 <Form.Field>
                     <label>Отчество</label>
-                    <Input fluid placeholder='Отчество' name='middlename' value={inputs.middlename} onChange={handleChange} />
+                    <Input fluid placeholder='Отчество' name='middlename' value={inputs.middlename} onChange={handleChange}/>
+                    {submitted && !inputs.middlename &&
+                        <div className="invalid-feedback">Введите Отчество*</div>
+                    }
                 </Form.Field>
                 <Form.Field>
                     <label>Фамилия</label>
                     <Input fluid placeholder='Фамилия' name='lastname' value={inputs.lastname} onChange={handleChange} />
+                    {submitted && !inputs.lastname &&
+                        <div className="invalid-feedback">Введите Фамилия*</div>
+                    }
                 </Form.Field>
             </Form.Group>
             <Form.Group widths='equal'>
                 <Form.Field>
                     <label>Номер телефона</label>
                     <Input fluid type="number" placeholder='Номер телефона' name='phone' value={inputs.phone} onChange={handleChange} />
+                    {submitted && !inputs.phone &&
+                        <div className="invalid-feedback">Введите Номер телефона*</div>
+                    }
                 </Form.Field>
                 <Form.Field>
                     <label>Адрес</label>
                     <Input fluid placeholder='Адрес' name='address' value={inputs.address} onChange={handleChange} />
+                    {submitted && !inputs.address &&
+                        <div className="invalid-feedback">Введите Адрес*</div>
+                    }
                 </Form.Field>
                 <Form.Field>
                     <label>ИНН</label>
                     <Input fluid type="number" placeholder='ИНН' name='tin' value={inputs.tin} onChange={handleChange} />
+                    {submitted && !inputs.tin &&
+                        <div className="invalid-feedback">Введите ИНН*</div>
+                    }
                 </Form.Field>
             </Form.Group>
             <Button type='submit'>Отправить</Button>

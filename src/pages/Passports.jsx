901672134960a56ea3ff368f7383e976d98ecd50
Alimov Tillo <alimovtillo@gmail.com>
2021-04-24 18:59:53 +0600

    fixed passports page

--- src/pages/Passports.jsx
+++ src/pages/Passports.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container, Header, Table, Loader } from 'semantic-ui-react'
+import { Container, Button, Table, Loader } from 'semantic-ui-react'
 
 
-// import { UserEditerModal } from '../components'
+// import { PassportsEditerModal } from '../components'
 import { passportsActions } from '../actions';
 
 export function Passports() {
@@ -15,27 +15,36 @@ export function Passports() {
     }, []);
 
     return (
-        <Container>
+        <Container style={{ marginTop: '50px' }}>
             {passports.loading && <Loader size='large'>Загрузка...</Loader>}
             {passports.error && <span className="text-danger">Ошибка: {passports.error}</span>}
-            {passports.data && <Table color='grey' compact celled striped selectable unstackable>
+            {passports.data && <Table color='grey' compact striped selectable unstackable>
 
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>ID</Table.HeaderCell>
+                        <Table.HeaderCell>ИНН</Table.HeaderCell>
+                        <Table.HeaderCell>Имя</Table.HeaderCell>
+                        <Table.HeaderCell>Фамилия</Table.HeaderCell>
+                        <Table.HeaderCell>Отчнство</Table.HeaderCell>
+                        <Table.HeaderCell>Номер телефона</Table.HeaderCell>
+                        <Table.HeaderCell>Адрес</Table.HeaderCell>
+                        <Table.HeaderCell/>
+                        <Table.HeaderCell/>
+                    </Table.Row>
+                </Table.Header>
                 <Table.Body>
                     {passports.data.map((user, index) =>
                         <Table.Row key={user.id}>
-                            <Table.Cell>
-                                <Header as='h4' image>
-                                    <Header.Content>
-                                        {user.tin}
-                                        <Header.Subheader>{user.id}</Header.Subheader>
-                                    </Header.Content>
-                                </Header>
-                            </Table.Cell>
+                            <Table.Cell>{user.id}</Table.Cell>
+                            <Table.Cell>{user.tin}</Table.Cell>
                             <Table.Cell>{user.firstname}</Table.Cell>
                             <Table.Cell>{user.lastname}</Table.Cell>
                             <Table.Cell>{user.middlename}</Table.Cell>
                             <Table.Cell>{user.phone}</Table.Cell>
                             <Table.Cell>{user.address}</Table.Cell>
+                            <Table.Cell collapsing><Button positive>Изменить</Button></Table.Cell>
+                            <Table.Cell collapsing><Button negative>Удалить</Button></Table.Cell>
                         </Table.Row>)}
                 </Table.Body>
             </Table>}
